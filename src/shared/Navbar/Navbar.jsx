c6bf0f63aad6dcaf0c5d58670eced53bd0530152
mushfiqueraiyan <mushfiqueraiyan1@gmail.com>
2025-09-24 22:37:55 +0600

    fixed reponsive section added  padding on navigation bar

--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -4,48 +4,50 @@ import { NavLink } from "react-router";
 
 const Navbar = () => {
   return (
-    <div className="flex items-center justify-between border-b pb-4 border-gray-300">
+    <div className="flex items-center justify-between border-b pb-4 px-4 md:px-3 lg:px-3 border-gray-300">
       <div>
         <h1 className="font-bold text-3xl flex items-center gap-2">
           <Footprints size={35} color="#297bff" /> Shoezzy
         </h1>
       </div>
-      <div className="flex items-center gap-10">
-        <NavLink
-          className={({ isPending, isActive }) =>
-            isPending
-              ? "pending.."
-              : isActive
-              ? "border-b-2 font-bold border-[#297bff]"
-              : "hover:border-b-2 hover:border-[#297bff] hover:transition hover:delay-75"
-          }
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to={"/about"}
-          className={({ isPending, isActive }) =>
-            isPending
-              ? "pending.."
-              : isActive
-              ? "border-b-2 font-bold border-[#297bff]"
-              : "hover:border-b-2 hover:border-[#297bff] hover:transition"
-          }
-        >
-          About
-        </NavLink>
-        <NavLink
-          to={"/shop"}
-          className={({ isPending, isActive }) =>
-            isPending
-              ? "pending.."
-              : isActive
-              ? "border-b-2 font-bold border-[#297bff]"
-              : "hover:border-b-2 hover:border-[#297bff] hover:transition hover:delay-75"
-          }
-        >
-          Shop
-        </NavLink>
+      <div className="flex items-center gap-8">
+        <div className=" items-center gap-10 hidden md:hidden lg:flex">
+          <NavLink
+            className={({ isPending, isActive }) =>
+              isPending
+                ? "pending.."
+                : isActive
+                ? "border-b-2 font-bold border-[#297bff]"
+                : "hover:border-b-2 hover:border-[#297bff] hover:transition hover:delay-75"
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to={"/about"}
+            className={({ isPending, isActive }) =>
+              isPending
+                ? "pending.."
+                : isActive
+                ? "border-b-2 font-bold border-[#297bff]"
+                : "hover:border-b-2 hover:border-[#297bff] hover:transition"
+            }
+          >
+            About
+          </NavLink>
+          <NavLink
+            to={"/shop"}
+            className={({ isPending, isActive }) =>
+              isPending
+                ? "pending.."
+                : isActive
+                ? "border-b-2 font-bold border-[#297bff]"
+                : "hover:border-b-2 hover:border-[#297bff] hover:transition hover:delay-75"
+            }
+          >
+            Shop
+          </NavLink>
+        </div>
         <div className="relative">
           <Heart size={30} />
           <span className=" bg-gray-200 text-[#297bff] rounded-full absolute -top-1 left-6 w-6 flex items-center justify-center h-6">
