208168bafdf907f73b609a2e2fc93a9addee0e0f
mushfiqueraiyan <mushfiqueraiyan1@gmail.com>
2025-10-07 23:34:17 +0600

    Installed Tanstack Query and implemented to fetch all products in the most popular product section by using axios

--- src/components/PopularProduct.jsx
+++ src/components/PopularProduct.jsx
@@ -1,7 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import { ShoppingCart } from "lucide-react";
 import React from "react";
 
 const PopularProduct = () => {
+  const { data: product, isLoading } = useQuery({
+    queryKey: ["product"],
+    queryFn: async () => {
+      const res = await axios.get("http://localhost:3000/products");
+      return res.data;
+    },
+  });
+
   return (
     <div className="mt-15 px-3">
       <h1 className="font-semibold text-2xl">Most Popular Products</h1>
@@ -9,42 +19,30 @@ const PopularProduct = () => {
       {/* Popular Product Section */}
 
       <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-5 my-8 ">
-        <div className="border rounded-lg border-gray-200 h-[17rem] hover:h-[20rem] transition-all duration-300 p-3 relative overflow-hidden group">
-          <img
-            src="https://demo.xpeedstudio.com/marketov2/shoe/wp-content/uploads/sites/15/2018/10/4-min-300x300.jpg"
-            alt=""
-            className="w-full h-40 object-cover"
-          />
-          <span className="text-gray-500">Sports Shoe</span>
-          <h3 className="text-xl font-semibold my-1">Nike Shorts Shoe 70</h3>
-          <p className="text-blue-500">$60.05</p>
+        {product?.map((p) => {
+          return (
+            <>
+              <div className="border rounded-lg border-gray-200 h-[17rem] hover:h-[20rem] transition-all duration-300 p-3 relative overflow-hidden group">
+                <img
+                  src={p.thumbnail}
+                  alt=""
+                  className="w-full h-35 object-contain mb-2"
+                />
+                <span className="text-gray-500 ">{p.category}</span>
+                <h3 className="text-xl font-semibold ">{p.name}</h3>
+                <p className="text-blue-500">${p.price}</p>
 
-          <button
-            className="flex items-center gap-2 btn bg-blue-500 text-white w-full mt-2 
+                <button
+                  className="flex items-center gap-2 btn bg-blue-500 text-white w-full mt-2 
                opacity-0 translate-y-2 group-hover:opacity-100 group-hover:translate-y-0 
                transition-all duration-300 ease-in-out"
-          >
-            <ShoppingCart /> Add To Cart
-          </button>
-        </div>
-        <div className="border rounded-lg border-gray-200 h-[17rem] hover:h-[20rem] transition-all duration-300 p-3 relative overflow-hidden group">
-          <img
-            src="https://demo.xpeedstudio.com/marketov2/shoe/wp-content/uploads/sites/15/2018/10/1-min-300x300.jpg"
-            alt=""
-            className="w-full h-40 object-cover"
-          />
-          <span className="text-gray-500">Sports Shoe</span>
-          <h3 className="text-xl font-semibold my-1">Nike Shorts Shoe 70</h3>
-          <p className="text-blue-500">$60.05</p>
-
-          <button
-            className="flex items-center gap-2 btn bg-blue-500 text-white w-full mt-2 
-               opacity-0 translate-y-2 group-hover:opacity-100 group-hover:translate-y-0 
-               transition-all duration-300 ease-in-out"
-          >
-            <ShoppingCart /> Add To Cart
-          </button>
-        </div>
+                >
+                  <ShoppingCart /> Add To Cart
+                </button>
+              </div>
+            </>
+          );
+        })}
       </div>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
 import { router } from "./routes/router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const query = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={query}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
