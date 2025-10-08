1c6ec60975d7a9729587694afe9e5647cdc005d9
mushfiqueraiyan <mushfiqueraiyan1@gmail.com>
2025-10-08 23:02:07 +0600

    Added Redux store for add product to the cart by state management

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,17 @@ import "./index.css";
 import { RouterProvider } from "react-router";
 import { router } from "./routes/router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 const query = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <QueryClientProvider client={query}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={query}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </Provider>
   </StrictMode>
 );
